5f9044128efabcaf507d9b3e63fa42653859acbc
Emmanuel Wineberg <esheyw@gmail.com>
2015-07-09 23:13:06 -0400

    merging www because fuck it

---
+++ www/html/index.php
@@ -0,0 +1,15 @@
+<?php
+#Page specific variables
+$USE_BOOTSTRAP = true;
+$PAGE_TITLE = 'Thallius\'s EVE Tools';
+?>
+<!DOCTYPE html>
+<html>
+<?php require_once 'head.html.php'; ?>
+    <body>
+    <pre><?php
+    var_dump($_SERVER);
+    ?>
+    </pre>
+    </body>
+</html>
